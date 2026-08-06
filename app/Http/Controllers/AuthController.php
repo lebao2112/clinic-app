936784c6f2401e5e14956ddc4186b67832c79b1c
LeeBaro <ledangtuanbao@gmail.com>
2026-08-06 10:17:51 +0700

    Update file AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,16 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-class AuthController extends Controller
-{
-    //
-}
-<?php
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 
