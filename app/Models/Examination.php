548f835cd6c87c329210e93a6bc6b8c862a196eb
LeeBaro <ledangtuanbao@gmail.com>
2026-08-18 09:53:16 +0700

    T3.8-T3.9: Setup database and auto-invoicing flow

--- app/Models/Examination.php
+++ app/Models/Examination.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Examination extends Model
 {
@@ -41,5 +42,10 @@ class Examination extends Model
     public function patient(): BelongsTo
     {
         return $this->belongsTo(Patient::class);
+    }
+
+    public function invoice(): HasOne
+    {
+        return $this->hasOne(Invoice::class);
     }
 }
